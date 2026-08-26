389159ecae9bf88a8b045407f91afc0407905b1d
Anant Negi <anantnegi04@gmail.com>
2026-08-26 07:04:42 +0530

    Modified login method to resetState to start from a fresh page each time a test executes

--- src/test/java/baseTest/BaseTests.java
+++ src/test/java/baseTest/BaseTests.java
@@ -1,29 +1,50 @@
 package baseTest;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.BaseComponents.LoginPage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseTests {
-    private WebDriver driver;
+    protected WebDriver driver;
     protected LoginPage loginPage;
+    private static final String BASE_URL = "https://opensource-demo.orangehrmlive.com/";
 
     @BeforeClass
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-features=PasswordLeakDetection,PasswordManagerOnboarding");
+        options.addArguments("--incognito");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
     }
 
     @BeforeMethod
-    public void goToLoginPage() {
-        driver.get("https://opensource-demo.orangehrmlive.com/");
+    public void resetState() {
+        driver.get(BASE_URL);
+        driver.manage().deleteAllCookies();
+        driver.get(BASE_URL);
+        loginPage = new LoginPage(driver);
     }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
