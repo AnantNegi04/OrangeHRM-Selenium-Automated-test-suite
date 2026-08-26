35ef37b1477be08d1fe0d21325c6bcd2b08cbbab
Anant Negi <anantnegi04@gmail.com>
2026-08-26 07:08:01 +0530

    Added tamperCsrfToken method to set the token value as arbitrary string for validating the middleware verification of the token

--- src/main/java/pages/BaseComponents/LoginPage.java
+++ src/main/java/pages/BaseComponents/LoginPage.java
@@ -1,11 +1,10 @@
 package pages.BaseComponents;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Dashboard.Dashboard;
+import pages.Leave.Components.TopBarMenu;
 
 import java.time.Duration;
 
@@ -13,6 +12,15 @@ public class LoginPage extends BasePage {
     private By userName = By.name("username");
     private By userPassword = By.xpath("//input[@type='password']");
     private By submit = By.xpath("//button[contains(normalize-space(.), 'Login')]");
+    private By errorMessage = By.xpath("//p[contains(@class, 'oxd-alert-content-text')]");
+    private By usernameRequiredError = By.xpath(
+            "//input[@name='username']/ancestor::div[contains(@class,'oxd-input-group')]" +
+                    "//span[contains(@class,'oxd-input-group__message')]"
+    );
+    private By passwordRequiredError = By.xpath(
+            "//input[@name='password']/ancestor::div[contains(@class,'oxd-input-group')]" +
+                    "//span[contains(@class,'oxd-input-group__message')]"
+    );
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -30,13 +38,48 @@ public class LoginPage extends BasePage {
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
 
-    public Dashboard login(String username, String password) {
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).getText();
+    }
+
+    public boolean isUsernameRequiredErrorDisplayed() {
+        try {
+            return wait.until(d -> !d.findElements(usernameRequiredError).isEmpty());
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isPasswordRequiredErrorDisplayed() {
+        try {
+            return wait.until(d -> !d.findElements(passwordRequiredError).isEmpty());
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isOnLoginPage() {
+        return driver.getCurrentUrl().contains("/auth/login");
+    }
+
+    public void attemptLogin(String username, String password) {
         find(userName).sendKeys(username);
         find(userPassword).sendKeys(password);
         click(submit);
+    }
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector(".oxd-navbar-nav")));
+    public Dashboard login(String username, String password) {
+        attemptLogin(username, password);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".oxd-layout-context")));
         return new Dashboard(driver);
+    }
+
+    public void tamperCsrfToken(String fakeValue) {
+        WebElement tokenInput = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.name("_token"))
+        );
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].value = arguments[1];", tokenInput, fakeValue
+        );
     }
 }
