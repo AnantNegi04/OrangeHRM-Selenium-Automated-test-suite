0be0ea363018c7c4735fb23f41aabb2f0030caaf
Anant Negi <anantnegi04@gmail.com>
2026-09-04 09:21:34 +0530

    changes commited

--- src/main/java/pages/Dashboard/Dashboard.java
+++ src/main/java/pages/Dashboard/Dashboard.java
@@ -1,10 +1,12 @@
 package pages.Dashboard;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pages.BaseComponents.BasePage;
 import pages.BaseComponents.SideBar;
 import pages.BaseComponents.TopBar;
 import pages.Leave.LeavePage;
+import pages.Maintenance.MaintenancePage;
 import pages.Time.TimePage;
 
 public class Dashboard extends BasePage {
@@ -28,5 +30,16 @@ public class Dashboard extends BasePage {
     public void logOut() {
         topBar = new TopBar(driver);
         topBar.getDropDownItem("Logout");
+    }
+
+    public boolean isDisplayed() {
+        sideBar = new SideBar(driver);
+        return !sideBar.searchHasNoResults("Admin");
+    }
+
+    public boolean maintenancePage() {
+        sideBar = new SideBar(driver);
+        MaintenancePage page = sideBar.goToMaintenancePage();
+        return page.isPasswordConfirmationDisplayed();
     }
 }
