f3e43c306678ca3aa4c9b349c29178d9d1d546ee
Anant Negi <anantnegi04@gmail.com>
2026-09-10 07:10:30 +0530

    Implemented quickLaunch page

--- src/main/java/pages/Dashboard/Components/QuickLaunch.java
+++ src/main/java/pages/Dashboard/Components/QuickLaunch.java
@@ -2,7 +2,15 @@ package pages.Dashboard.Components;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.BaseComponents.BasePage;
+import pages.Leave.NavPages.ApplyLeave;
+import pages.Leave.NavPages.AssignLeave;
+import pages.Leave.NavPages.LeaveList;
+import pages.Leave.NavPages.MyLeave;
+import pages.Time.NavPages.Timesheets.EmployeeTimesheets;
+import pages.Time.NavPages.Timesheets.MyTimesheets;
 
 public class QuickLaunch extends BasePage {
 
@@ -15,5 +23,55 @@ public class QuickLaunch extends BasePage {
 
     public QuickLaunch(WebDriver driver) {
         super(driver);
+    }
+
+    private WebElement getElement(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return driver.findElement(locator);
+    }
+
+    private By getLocator(String locator) {
+        return By.xpath(
+                "//button[@title='" + locator + "']"
+        );
+    }
+
+    public void navigateQuickLaunch(String locator) {
+        By navLocator = getLocator(locator);
+        getElement(navLocator).click();
+
+        wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("oxd-layout-container"))
+        );
+    }
+
+    public AssignLeave getAssignLeave() {
+        getElement(assignLeave).click();
+        return new AssignLeave(driver);
+    }
+
+    public LeaveList getLeaveList() {
+        getElement(leaveList).click();
+        return new LeaveList(driver);
+    }
+
+    public MyLeave getMyLeave() {
+        getElement(myLeave).click();
+        return new MyLeave(driver);
+    }
+
+    public MyTimesheets getMyTimesheets() {
+        getElement(myTimeSheet).click();
+        return new MyTimesheets(driver);
+    }
+
+    public ApplyLeave getApplyLeave() {
+        getElement(applyLeave).click();
+        return new ApplyLeave(driver);
+    }
+
+    public EmployeeTimesheets getEmployeeTimesheets() {
+        getElement(timeSheets).click();
+        return new EmployeeTimesheets(driver);
     }
 }
