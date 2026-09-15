faecd1f636e5ac24d6cd898b6bd6f0e12753452c
Anant Negi <anantnegi04@gmail.com>
2026-09-15 06:53:04 +0530

    Changed assign leave implementation to interact with dynamic states of DOM

--- src/main/java/pages/Leave/NavPages/AssignLeave.java
+++ src/main/java/pages/Leave/NavPages/AssignLeave.java
@@ -1,9 +1,6 @@
 package pages.Leave.NavPages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.BaseComponents.BasePage;
 import pages.BaseComponents.SideBar;
@@ -19,19 +16,34 @@ public class AssignLeave extends BasePage {
     private By inputFromDate = By.xpath("//label[normalize-space()='From Date']/ancestor::div[contains(@class,'oxd-input-group')]//input");
     private By inputToDate = By.xpath("//label[normalize-space()='To Date']/ancestor::div[contains(@class,'oxd-input-group')]//input");
     private By leaveTypeDropdown = By.xpath("//label[normalize-space()='Leave Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class, 'oxd-select-text-input')]");
-    private By autoComplete = By.cssSelector("div[role='listbox'][class*='oxd-autocomplete-dropdown']");
-    private By clearMultipleSelect = By.xpath("//i[contains(@class, '--clear')]");
     private By assignButton = By.xpath("//button[@type='submit']");
     private By commentBox = By.xpath("//textarea[contains(@class, 'oxd-textarea')]");
     private By employeeName = By.xpath("//input[contains(@placeholder, 'Type for hints...')]");
     private By leavebalance = By.xpath("//div[contains(@class, 'oxd-input-group')]/ancestor::div//p[contains(@class, 'orangehrm-leave-balance-text')]");
+    private By partialDays = By.xpath("//label[normalize-space()='Partial Days']/ancestor::div[contains(@class, 'oxd-input-group')]//div[contains(@class, 'oxd-select-text-input')]");
+    private By duration = By.xpath("//label[normalize-space()='Duration']/ancestor::div[contains(@class, 'oxd-input-group')]//div[contains(@class, 'oxd-select-text-input')]");
+    private By confirmButton = By.xpath("//button[normalize-space()='Ok']");
+    private By cancelButton = By.xpath("//button[normalize-space()='Cancel']");
+    private By overlappingTitle = By.xpath("//h6[normalize-space()='Overlapping Leave Request(s) Found']");
+    //private By recordsFound = By.xpath("//span[contains(@class, 'oxd-text--span')][text()=' (5) Records Found']");
+    private By overlappingRecords = By.xpath("//div[contains(@class, 'oxd-table-card')]");
 
     public AssignLeave(WebDriver driver) {
         super(driver);
     }
 
     private WebElement getElements(By name) {
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(name)
+        );
         return driver.findElement(name);
+    }
+
+    private By dropdown(String option) {
+        return By.xpath(
+                "//div[contains(@class,'oxd-select-option')]" +
+                        "[normalize-space()='" + option + "']"
+        );
     }
 
     private void enterFromDate(String date) {
@@ -53,58 +65,112 @@ public class AssignLeave extends BasePage {
         element.sendKeys(date);
     }
 
-    private By leaveTypeOption(String type) {
-        return By.xpath(
-                "//div[contains(@class,'oxd-select-option')]" +
-                        "[normalize-space()='" + type + "']"
-        );
-    }
-
     public void selectDropDownLeaveType(String type) {
 
         getElements(leaveTypeDropdown).click();
 
         WebElement option = wait.until(
                 ExpectedConditions.elementToBeClickable(
-                        leaveTypeOption(type)
+                        dropdown(type)
                 )
         );
 
         option.click();
     }
 
-    public void setEmployeeName() {
-        getElements(employeeName).sendKeys("A");
+    private By employeeNameOption(String employeeName) {
+        return By.xpath(
+                "//div[@role='listbox']" +
+                        "[normalize-space()='" + employeeName + "']"
+        );
+    }
 
-        WebElement firstName = wait.until(
-                ExpectedConditions.elementToBeClickable(autoComplete)
+    public void setEmployeeName(String eName) {
+        getElements(employeeName).sendKeys(eName);
+
+        WebElement name = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        employeeNameOption(eName)
+                )
         );
 
-        firstName.click();
+        name.click();
     }
+
 
     public void enterComments(String comments) {
         getElements(commentBox).sendKeys(comments);
     }
 
-    private boolean canAssign() {
-        if (getElements(leavebalance).getText().equals("Balance not sufficient")) {
+    private boolean isInsufficientBalanceDialogDisplayed() {
+        try {
+            return wait.until(d -> !d.findElements(confirmButton).isEmpty());
+        } catch (TimeoutException e) {
             return false;
         }
-        return true;
     }
 
-    public void assignLeave(String leaveType, String FromDate, String ToDate, String comments) {
+    public void clickSubmit() {
+        getElements(assignButton).click();
+    }
+
+    public void clickConfirm() {
+        getElements(confirmButton).click();
+    }
+
+    public void clickCancel() {
+        getElements(cancelButton).click();
+    }
+
+    public void selectPartialDays(String days) {
+        getElements(partialDays).click();
+
+        WebElement option = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        dropdown(days)
+                )
+        );
+
+        option.click();
+    }
+
+    public void selectDuration(String d) {
+        getElements(duration).click();
+
+        WebElement option = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        dropdown(d)
+                )
+        );
+
+        option.click();
+    }
+
+    public boolean isOverlapping() {
+        try {
+            return wait.until(d -> !d.findElements(overlappingTitle).isEmpty());
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+
+    public boolean assignLeave(String leaveType, String FromDate, String ToDate, String eName, String comments) {
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("oxd-layout-context")));
-        setEmployeeName();
+        setEmployeeName(eName);
         selectDropDownLeaveType(leaveType);
         enterFromDate(FromDate);
         enterToDate(ToDate);
-        if (!canAssign()) {
-            System.out.println("cannot assign leave type due to insufficient balance");
-            return;
-        }
         enterComments(comments);
-        getElements(assignButton).click();
+        clickSubmit();
+
+        if (isInsufficientBalanceDialogDisplayed()) {
+            clickConfirm();
+        }
+
+        if (isOverlapping()) {
+            return false;
+        }
+        return true;
     }
 }
